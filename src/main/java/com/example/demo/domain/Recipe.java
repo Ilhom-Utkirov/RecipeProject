08467d52f6c2806552415d8d587dfe5957840e73
O'tkirov Ilhom <inspiration95@mail.ru>
2021-01-17 21:42:06 +0500

    added Difficulty enum and declared it in the class

--- src/main/java/com/example/demo/domain/Recipe.java
+++ src/main/java/com/example/demo/domain/Recipe.java
@@ -26,6 +26,9 @@ public class Recipe {
     @Lob //binary large field is created (Blob field)
     private Byte[] image;
 
+    @Enumerated(value = EnumType.STRING)
+    private  Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
