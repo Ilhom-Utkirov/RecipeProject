6ae7a8fb62600f7547bcf34cb66b857a8a044730
O'tkirov Ilhom <inspiration95@mail.ru>
2021-01-24 17:46:36 +0500

    added Spring Jpa Jqueries for database

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package com.example.demo.controllers;
 
+import com.example.demo.domain.Category;
+import com.example.demo.domain.UnitOfMeasure;
+import com.example.demo.repositories.CategoryRepository;
+import com.example.demo.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPAge(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("Mexican");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Tablespoon");
+
+        System.out.println("Cat Id is:"+categoryOptional.get().getId());
+        System.out.println("unit of Measure ID is:"+ unitOfMeasure.get().getId());
         return "index";
     }
 
