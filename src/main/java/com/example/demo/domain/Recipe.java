ff9a3a74aaf7f4f146356ad828a736dbbaef965c
O'tkirov Ilhom <inspiration95@mail.ru>
2021-02-02 23:19:04 +0500

    added lombok @Data

--- src/main/java/com/example/demo/domain/Recipe.java
+++ src/main/java/com/example/demo/domain/Recipe.java
@@ -1,9 +1,12 @@
 package com.example.demo.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Data
 @Entity
 public class Recipe {
 
--- src/main/java/com/example/demo/services/RecipeServiceImpl.java
+++ src/main/java/com/example/demo/services/RecipeServiceImpl.java
@@ -2,10 +2,13 @@ package com.example.demo.services;
 
 import com.example.demo.domain.Recipe;
 import com.example.demo.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
+
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -17,8 +20,10 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
-        Set<Recipe> recipeSet = new HashSet<>();
 
+        log.debug("I'm in the service");
+
+        Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
     }
