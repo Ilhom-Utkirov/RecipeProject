f7187d6b43d28d4a7823afef7208d342f753335c
Ilhom-Utkirov <Inspiration9595@gmail.com>
2021-02-10 19:25:57 +0500

    didnot finish 174 lesson finish it at home

---
+++ src/test/java/com/example/demo/controllers/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.controllers;
+
+import com.example.demo.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.*;
+
+public class IndexControllerTest {
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+     @Before
+     public void setUp(){
+
+     }
+
+    @Test
+    public void getIndexPAge() {
+
+
+
+
+    }
+}
