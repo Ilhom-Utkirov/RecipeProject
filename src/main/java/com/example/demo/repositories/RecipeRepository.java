953356fc5aa4685ea92d5d78500a118c46bc81e3
O'tkirov Ilhom <inspiration95@mail.ru>
2021-02-23 01:00:24 +0500

    added show page

--- src/main/java/com/example/demo/repositories/RecipeRepository.java
+++ src/main/java/com/example/demo/repositories/RecipeRepository.java
@@ -3,5 +3,10 @@ package com.example.demo.repositories;
 import com.example.demo.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+
+
+
 }
